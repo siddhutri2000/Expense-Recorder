655adfd525c71b2b0c610ff936d8fe0c423a7f4a
Sid <siddhu.tri2000@gamil.com>
2021-12-07 02:04:26 +0530

    Added data Dyanmically

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,21 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  // const [title, setTitle] = useState(props.title);
 
   // let title = props.title;
-  const clickHandler = () => {
-    setTitle(prompt("enter the new title"));
-  };
+  // const clickHandler = () => {
+  //   // setTitle(prompt("enter the new title"));
+  // };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      <button>Change Title</button>
     </Card>
   );
 };
